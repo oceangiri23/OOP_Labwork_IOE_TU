2e214da07458df94ddb163bcbe09f475892e30d2
Sagar Giri <oceangiri5656@gmail.com>
2023-07-14 19:39:16 +0545

    Lab five done

--- Lab_Sheet-5_Understanding the Concept of Friend FunctionClass and Operator Overloading/no_5-3.cpp
+++ Lab_Sheet-5_Understanding the Concept of Friend FunctionClass and Operator Overloading/no_5-3.cpp
@@ -1,7 +1,7 @@
 /*Write a program to compare two objects of a class that contains an integer
 value as its data member. Make overloading functions to overload equality(==),
- less than(<), greater than(>), not equal (!=), greater than or equal to (>=), and
- less than or equal to(<=) operators using member operator functions.*/
+less than(<), greater than(>), not equal (!=), greater than or equal to (>=), and
+less than or equal to(<=) operators using member operator functions.*/
 
 #include <iostream>
 using namespace std;
